adab4ec70a4544f4c35268070313ca8be41932b2
John <jddcp@ic.ufal.br>
2020-07-27 22:22:33 -0300

    cannot GET bug solved

--- src/app/task/task.page.ts
+++ src/app/task/task.page.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { AlertController } from '@ionic/angular';
-import { profile } from 'console';
 
 @Component({
   selector: 'app-task',
@@ -11,11 +7,7 @@ import { profile } from 'console';
 })
 export class TaskPage implements OnInit {
   
-  constructor(){
+  constructor(){ }
 
-  }
-
-  ngOnInit(){
-    
-  }
+  ngOnInit(){ }
 }
